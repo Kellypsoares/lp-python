c854a40cf0b6f9972ca2d0980e018de16322fe39
Kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-09-22 09:38:43 -0300

    começando o cap 4

---
+++ book-ericmatthes/chapter/chapter-4.py
@@ -0,0 +1,6 @@
+
+teachers = ["xandão", "regina", "aline","ronaldo"]
+for teacher in teachers: 
+        print(teacher.title() + ", thank you for all!")        
+        print("I can't wait to see you again, " + teacher.title() + ".\n")
+
