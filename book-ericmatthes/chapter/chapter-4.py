bdef02374a1acc69e3a8c38709600cb901efea2e
Kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-09-22 12:12:57 -0300

    mais um pouco

--- book-ericmatthes/chapter/chapter-4.py
+++ book-ericmatthes/chapter/chapter-4.py
@@ -3,4 +3,14 @@ teachers = ["xandão", "regina", "aline","ronaldo"]
 for teacher in teachers: 
         print(teacher.title() + ", thank you for all!")        
         print("I can't wait to see you again, " + teacher.title() + ".\n")
+print("You are the best")
 
+
+
+for value in range(1,5):
+        print(value)
+
+numbers = list(range(1,5))
+print(numbers)
+even_numbers = list(range(2,11,3))
+print(even_numbers)
