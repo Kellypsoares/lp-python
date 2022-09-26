721375cb2d9e127938437f152778a8424146f543
Kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-09-26 11:36:09 -0300

    laço for

--- book-ericmatthes/chapter/chapter-4.py
+++ book-ericmatthes/chapter/chapter-4.py
@@ -14,3 +14,15 @@ numbers = list(range(1,5))
 print(numbers)
 even_numbers = list(range(2,11,3))
 print(even_numbers)
+
+squares = []
+for value in range(1,11): 
+        square = value**2
+        squares.append(square)
+        print(squares)
+
+squares = []
+for value in range(1,11): 
+        squares.append(value**2)
+        print(squares)
+        
