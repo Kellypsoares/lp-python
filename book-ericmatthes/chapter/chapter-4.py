52e80ebdf95b26718d771716f7a4ce9d2d61f7a8
Kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-09-27 11:54:31 -0300

    cap 4 quase acabando

--- book-ericmatthes/chapter/chapter-4.py
+++ book-ericmatthes/chapter/chapter-4.py
@@ -25,6 +25,44 @@ squares = []
 for value in range(1,11): 
         squares.append(value**2)
         print(squares)
-        
-players = ["henrique", "bruno","kelly", "pedro", "andressa"]
+ #Uma list comprehension (abrangência de lista) permite gerar essa mesma lista com apenas uma linha de código. Uma list comprehension combina o laço for e a criação de novos elementos em uma linha, e concatena cada novo elemento automaticamente.       
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+
+#metódo pra encontrar o valor mínimo, máximo e o somatório de uma lista
+
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(min(digits)) 
+print(max(digits))
+print(sum(digits))
+
+
+#Para criar uma fatia, especifique o índice do primeiro e do último elemento 
+# ...com os quais você quer trabalhar. 
+# ...Como ocorre na função range(), Python para em um item
+# ...antes do segundo índice que você especificar. 
+# ...Para exibir os três primeiros elementos de uma lista, 
+# ...solicite os índices de 0 a 3; os elementos 0, 1 e 2 serão devolvidos.
+students = ["henrique", "bruno","kelly", "pedro", "andressa"]
+print(students[0:3])
 print("Here are the first three players on my team")
+
+#Se o primeiro índice de uma fatia for omitido, 
+#...Python começará sua fatia automaticamente no início da lista:
+
+print(students [:4])
+# o oposto também é verdade 
+print(students[2:])
+
+#um índice negativo devolve um elemento a uma 
+#...determinada distância do final de uma lista
+print(students[-3:])
+
+#Percorrendo uma fatia com um laço
+
+students = ["henrique", "bruno","kelly", "pedro", "andressa"]
+print("Here are the first three students on my class")
+for student in students[0:3]:
+        print(student.title())
+
