89ded2ac1db68572d9251f20b64579de1e671016
Kellypsoares <kellypinheirosoares@poli.ufrj.br>
2022-09-29 11:52:48 -0300

    começando cap 5

---
+++ book-ericmatthes/chapter/chapter-5.py
@@ -0,0 +1,9 @@
+cars = ["audi","bmw","toyota","ford"]
+for car in cars:
+        if car == "bmw": print(car.upper())
+        else: print(car.title())
+
+#python usa os valores True e False para decidir 
+#se o código em uma instrução if deve ser executado
+# sinal de igualdade duplo, como o que está emv, 
+# faz uma pergunta: “O valor de car é igual a 'bmw'?”.
